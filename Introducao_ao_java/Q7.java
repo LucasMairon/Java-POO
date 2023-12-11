ba51d4fab16ea9ec0ce1b1a3c2f5f392058ca966
Lucas Mairon <120278386+LucasMairon@users.noreply.github.com>
2023-12-11 14:09:15 -0300

    adicionando questao 7

---
+++ Introducao_ao_java/Q7.java
@@ -0,0 +1,19 @@
+package Introducao_ao_java;
+
+import java.util.Scanner;
+
+public class Q7 {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("digite 3 numeros:");
+        int primeiroNumero = scan.nextInt();
+        int segundoNumero = scan.nextInt();
+        int terceiroNumero = scan.nextInt();
+        int menorNumero = Math.min(primeiroNumero, segundoNumero);
+        menorNumero = Math.min(terceiroNumero, menorNumero);
+        System.out.println("o menor numero é: " + menorNumero);
+
+        scan.close();
+    }
+}
