44d1fb75b537b2887e4e26f25d316a3cd89e8ad7
Lucas Mairon <120278386+LucasMairon@users.noreply.github.com>
2023-12-11 13:56:31 -0300

    adicionando questoes 5 e 6 da lista

---
+++ Introducao_ao_java/Q5.java
@@ -0,0 +1,25 @@
+package Introducao_ao_java;
+import java.util.Scanner;
+
+public class Q5{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("digite o numero no formato CDU: ");
+        int numeroNoFormatoCDU = scan.nextInt();
+        int numeroNoFormatoUCD = 0;
+        numeroNoFormatoUCD = inverterNumerosRecursivo(numeroNoFormatoCDU,numeroNoFormatoUCD);
+        System.out.println("numero invertido: " + numeroNoFormatoUCD);
+        scan.close();
+                
+    }
+
+    
+    public static int inverterNumerosRecursivo(int inverter, int invertido){
+        if ((inverter % 10) == 0)
+            return invertido;
+        return  inverterNumerosRecursivo(inverter/10,invertido * 10 + (inverter % 10) );
+    }
+
+    
+    
+}
---
+++ Introducao_ao_java/Q6.java
@@ -0,0 +1,22 @@
+package Introducao_ao_java;
+
+import java.util.Scanner;
+
+public class Q6 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("digite o intervalo em minutos: ");
+        int tempoEmMinutosGeral = scan.nextInt();
+        System.out.print(tempoEmMinutosGeral + " = ");
+        int tempoEmDias,tempoEmHoras,tempoEmMinutos;
+        tempoEmDias = (tempoEmMinutosGeral/(24*60));
+        tempoEmMinutosGeral = (tempoEmMinutosGeral%(24*60));
+        tempoEmHoras = (tempoEmMinutosGeral / 60);
+        tempoEmMinutosGeral = (tempoEmMinutosGeral % 60);
+        tempoEmMinutos = tempoEmMinutosGeral;
+        System.out.println(tempoEmDias + " dias " + tempoEmHoras + " horas " + tempoEmMinutos + " minutos");
+
+
+        scan.close();
+    }
+}
