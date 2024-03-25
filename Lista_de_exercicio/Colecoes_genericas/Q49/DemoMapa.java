6045ab648ee024d5ee523ecb7c60f0bfad3079cc
Lucas Mairon <120278386+LucasMairon@users.noreply.github.com>
2024-03-25 12:20:13 -0300

    atualizando questao 49

--- Lista_de_exercicio/Colecoes_genericas/Q49/DemoMapa.java
+++ Lista_de_exercicio/Colecoes_genericas/Q49/DemoMapa.java
@@ -3,27 +3,28 @@ import java.util.TreeMap;
 import java.util.Scanner;
 
 public class DemoMapa {
+
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         TreeMap<Character,Integer>caracteres = new TreeMap<>();
         System.out.print("Digite o texto: ");
         String texto = scan.nextLine();
         Character letra;
-        int valor = 1;
+
         for(int i = 0; i < texto.length(); i++){
             letra = texto.charAt(i);
-            if(caracteres.containsKey(letra)){
-                caracteres.put(letra,valor++);
-            }else{
-                caracteres.put(letra, valor);
+            if(letra != ' '){
+                if(caracteres.containsKey(letra)){
+                    caracteres.put(letra,caracteres.get(letra) + 1);
+                }else{
+                    caracteres.put(letra, 1);
+                }
             }
         }
 
-        for(int i = 0; i < caracteres.size();i++){
-            letra = texto.charAt(i);
-            System.out.println(caracteres.get(letra) + ": " + caracteres.get(letra).valueOf(i));
-        }
+        System.out.println(caracteres);
 
         scan.close();
     }
+    
 }
